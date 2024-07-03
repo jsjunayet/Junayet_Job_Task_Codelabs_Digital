d18ced0a79a914cbd4f080081fc7b4c05b34a7ed
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-07-03 18:07:17 +0600

    navbar and banar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      extend: {
+          fontFamily: {
+              inter: ['Inter', 'sans-serif'],
+          },
+      },
+  },
+  plugins: [],
+};
